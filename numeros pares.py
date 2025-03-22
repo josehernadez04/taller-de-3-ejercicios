83b4080f93c62b9e2a7e7279e9b240dca980af2f
josehernadez04 <josehernandez03b@gmail.com>
2025-03-21 20:58:54 -0500

    v2.00
    se actualiza el ejercicio de los números pares  ya que no cumplía con los requisitos
    los otros dos revisar.

--- numeros pares.py
+++ numeros pares.py
@@ -1,16 +1,26 @@
+def tomar_datos():
+    numero=int(input(" regalame un numero "))
+    return numero 
+    
+def procedimiento(numero):
+    return numero % 2 == 0
 
-def es_par(numero):
-    if numero % 2 == 0:
-        return True
+def resultado(numero):
+    if procedimiento(numero):
+        print(" el numero es par ")
     else:
-        return False
+        print(" el numero es impar ")
+def menu():
+    print(" 1. para saber si el numero es par o impar ")
+    print(" 2. para salir ")
+    opc=int(input(" digites el numero "))
+    return opc
 while True:
-    try:
-        numero = int(input("Ingresa un número: "))
-        break
-    except ValueError:
-        print("Por favor, ingresa un número válido.")
-if es_par(numero):
-    print("El número es par.",numero) 
-else:
-    print("El número es impar.",numero)
+    opcion=menu()
+    if opcion ==1:
+        numero=tomar_datos()
+        resultado(numero)
+    elif opcion==2:
+        print(" hasta luego ")
+    else:
+        print(" opcion incorrecta ")       
